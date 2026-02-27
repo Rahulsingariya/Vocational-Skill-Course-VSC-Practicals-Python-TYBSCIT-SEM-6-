7ad52c36f597077acb2460faf33c42047294a780
Rahul Singariya <rahulsingariya40912@gmail.com>
2026-02-27 21:35:15 +0530

    Update readnum.py

---
+++ readnum.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number (-1 to stop): "))
+
+while num != -1:
+    print("You entered:", num)
+    num = int(input("Enter a number (-1 to stop): "))
+
+print("Program terminated")
