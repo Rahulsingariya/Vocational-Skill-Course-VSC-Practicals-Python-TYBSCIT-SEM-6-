dfc11e2304a7b2cd664deb0c4a145a4c282a095c
Rahul Singariya <rahulsingariya40912@gmail.com>
2026-02-27 21:23:43 +0530

    Update character.py

---
+++ character.py
@@ -0,0 +1,8 @@
+ch = input("Enter a character: ")
+
+if ch.isalpha():
+    print("Character is an Alphabet")
+elif ch.isdigit():
+    print("Character is a Digit")
+else:
+    print("Character is a Special Character")
