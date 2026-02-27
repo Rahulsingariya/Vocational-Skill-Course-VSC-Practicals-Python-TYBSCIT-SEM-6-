dbc8c5c952bafd10eff033fafb6d676e8309df86
Rahul Singariya <rahulsingariya40912@gmail.com>
2026-02-27 21:37:50 +0530

    Update studresult.py

---
+++ studresult.py
@@ -0,0 +1,5 @@
+exam = float(input("Enter exam marks: "))
+sports = float(input("Enter sports marks: "))
+activity = float(input("Enter activity marks: "))
+result = exam*0.50 + sports*0.20 + activity*0.30
+print("Final result =", result)
