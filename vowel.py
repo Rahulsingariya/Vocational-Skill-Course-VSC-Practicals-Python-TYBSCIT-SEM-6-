fca240f4388b93a96663fe7e61faf602163766dc
Rahul Singariya <rahulsingariya40912@gmail.com>
2026-02-27 21:40:40 +0530

    Update vowel.py

---
+++ vowel.py
@@ -0,0 +1,6 @@
+ch = input("Enter a character: ")
+
+if ch.lower() == 'a' or ch.lower() == 'e' or ch.lower() == 'i' or ch.lower() == 'o' or ch.lower() == 'u':
+    print("Character is a Vowel")
+else:
+    print("Character is not a Vowel")
