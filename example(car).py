62a8fea1493b4bb205f00fd502195b68b825ce97
Rahul Singariya <rahulsingariya40912@gmail.com>
2026-02-27 21:27:26 +0530

    Update example(car).py

---
+++ example(car).py
@@ -0,0 +1,6 @@
+speed = int(input("Enter car speed (mph): "))
+
+if speed >= 45 and speed <= 70:
+    print("Speed is within the freeway limit")
+else:
+    print("Speed is outside the freeway limit")
