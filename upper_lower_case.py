dca4ec7c7242e30cdf50f25f7f5af70b01f50a11
Rahul Singariya <rahulsingariya40912@gmail.com>
2026-02-27 21:40:05 +0530

    Update upper_lower_case.py

---
+++ upper_lower_case.py
@@ -0,0 +1,8 @@
+ch = input("Enter a character: ")
+
+if ch.isupper():
+    print("Character is Uppercase")
+elif ch.islower():
+    print("Character is Lowercase")
+else:
+    print("Invalid input")
