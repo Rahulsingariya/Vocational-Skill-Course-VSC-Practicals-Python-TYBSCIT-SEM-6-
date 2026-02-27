8075621e3f473ddbbd977ec23745dd345663fd76
Rahul Singariya <rahulsingariya40912@gmail.com>
2026-02-27 21:25:12 +0530

    Update deviation.py

---
+++ deviation.py
@@ -0,0 +1,6 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+avg = (a + b) / 2
+print("Average =", avg)
+print("Deviation of first number =", a - avg)
+print("Deviation of second number =", b - avg)
